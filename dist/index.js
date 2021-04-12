e2996ee347bae5dacc5aa64ace261620947a9fe9
DESKTOP-OKR5BMP\etoos <a02t02@gmail.com>
2021-04-12 16:47:15 +0900

    ts setting

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// const name = "chesedi",
+// age = 24,
+// gender = "male";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `hello ${name}, you ar ${age}, you are a ${gender}!!!@@@`;
+};
+console.log(sayHi("chesedi", 24, "male"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// const name = "chesedi",
+// age = 24,
+// gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `hello ${name}, you ar ${age}, you are a ${gender}!!!@@@`;
+}
+
+console.log(sayHi("chesedi", 24, "male"));
+
+export {};
